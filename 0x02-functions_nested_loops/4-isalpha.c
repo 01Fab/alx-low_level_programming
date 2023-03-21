b4cc41606b48fe87d87957cad25f81dd8d148c33
Fabrice <uwafabrice01@gmail.com>
2023-03-21 06:56:42 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,25 +1,15 @@
 #include "holberton.h"
 /**
- * _isalpha - Check Main
- * @c: An input character
- * Description: function returns 1 if the character is a
- * letter, lowercase or uppercase.
- * Return: 1 or 0 in otherwise.
+ * _isalpha - tests whether a character is from the English alphabet.
+ * @c: character to be checked.
+ * Return: 1 if the character is an English character.
+ * 0 if the character is not an English character.
  */
 int _isalpha(int c)
 {
-	char lower, upper;
-	int isletter = 0;
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 
-	for (lower = 'a'; lower <= 'z'; lower++)
-	{
-		for (upper = 'A'; upper <= 'Z'; upper++)
-		{
-			if (c == lower || c == upper)
-				isletter = 1;
-		}
-	}
-	return (isletter);
-}[master 1c29294] #include "holberton.h" /**  * _isalpha - Check Main  * @c: An input character  * Description: function returns 1 if the character is a  * letter, lowercase or uppercase.  * Return: 1 or 0 in otherwise.  */ int _isalpha(int c) { char lower, upper; int isletter = 0;
- 1 file changed, 23 insertions(+)
- create mode 100644 0x02-functions_nested_loops/4-isalpha.c
+}
