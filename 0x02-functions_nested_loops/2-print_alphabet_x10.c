1e61900cda5f8658472c58a1cfaa141ab2dc5fc3
Fabrice <uwafabrice01@gmail.com>
2023-03-21 06:16:14 -0700

    3-islower.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,3 @@
-#include "main.h"
-
-/**
- * print_alphabet_x10 - Check Main
- * Description: function uses _putchar function to print
- * alphabet in lowercase 10 times
- * Return: Nothing.
- */
-
-void print_alphabet_x10(void)
-{
-	char i;
-	int j;
-
-	for (j = 0; j < 10; j++)
-	{
-		for (i = 'a'; i <= 'z'; i++)
-			_putchar(i);
-		_putchar('\n');
-	}
-}
+[master 342e575] #include "main.h"
+ 2 files changed, 24 insertions(+), 19 deletions(-)
+ create mode 100644 0x02-functions_nested_loops/2-print_alphabet_x10.c
